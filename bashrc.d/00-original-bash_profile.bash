8ff6aa9815aa7a8ed3936a9567e2feb458ab2c73
Kirby Powell <powell.ki@husky.neu.edu>
2018-02-03 12:05:01 -0800

    starting to add some linux specific touches..

--- bashrc.d/00-original-bash_profile.bash
+++ bashrc.d/00-original-bash_profile.bash
@@ -1,9 +1,12 @@
 
 # Global stuff
-export PATH=$HOME/bin:$PATH
+# export PATH=$HOME/bin:$PATH
 export EDITOR=vi
 
-# Homebrew stuff
-export PATH=$HOME/homebrew/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/homebrew/lib:$LD_LIBRARY_PATH
-alias ll="ls -al"
+#######################################
+# Homebrew stuff		      #
+# This Section only applies on Mac OS #
+#######################################
+# export PATH=$HOME/homebrew/bin:$PATH
+# export LD_LIBRARY_PATH=$HOME/homebrew/lib:$LD_LIBRARY_PATH
+# alias ll="ls -al"
