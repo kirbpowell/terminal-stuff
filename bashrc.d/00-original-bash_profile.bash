b75d3c8957e5410bd9edf9fb195b8e6409bc4276
Kirby Powell <kirbpowell@users.noreply.github.com>
2019-02-16 18:57:43 -0800

    Add to ls alias

--- bashrc.d/00-original-bash_profile.bash
+++ bashrc.d/00-original-bash_profile.bash
@@ -3,10 +3,12 @@
 # export PATH=$HOME/bin:$PATH
 export EDITOR=vi
 
+# alias ls and make it pretty
+alias ll="ls -alGF"
+
 #######################################
 # Homebrew stuff		      #
 # This Section only applies on Mac OS #
 #######################################
 # export PATH=$HOME/homebrew/bin:$PATH
 # export LD_LIBRARY_PATH=$HOME/homebrew/lib:$LD_LIBRARY_PATH
-# alias ll="ls -al"
