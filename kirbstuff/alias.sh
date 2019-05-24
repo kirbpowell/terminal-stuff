4816f0d9e95c8d6d73944b69849da4aecdd7b7d6
Kirby Powell <kirbpowell@users.noreply.github.com>
2019-05-24 11:03:43 -0700

    Adding logically separated shell scripts

---
+++ kirbstuff/alias.sh
@@ -0,0 +1,13 @@
+# Kirby's Aliases!
+
+# prettier git log - credit to coderwall.com
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias vim="/usr/local/bin/nvim"
+alias vi="/usr/local/bin/nvim"
+
+# Kitty shell's neat difftool thing.
+alias kdiff="git difftool --no-symlinks --dir-diff"
+
+# Reload Chunkwm settings
+alias chunkrl="brew services restart chunkwm"
